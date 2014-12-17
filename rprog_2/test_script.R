c89f2f5e71fb258fd890ea43c95df9bb037165eb
Jiahong Chen <jrpg0618@gmail.com>
2014-12-17 23:02:26 +0800

    Assignment2 added

--- rprog_2/test_script.R
+++ rprog_2/test_script.R
@@ -9,4 +9,16 @@ x <- c(rnorm(10), runif(10), rnorm(10, 1))
 f<-gl(3,10)
 split(x, f)
 
+s <- split(iris, iris$Species)
+meanFunc <- function(x) colMeans(x[, c("Sepal.Length","Sepal.Width", 
+                                       "Petal.Length","Petal.Width")])
+meanFunc2 <- function(x) colMeans(x[,"Sepal.Length"])
+lapply(s, meanFunc)
+lapply(s, meanFunc2)
 
+
+library(datasets)
+data(mtcars)
+tapply(mtcars$mpg, mtcars$cyl, mean)
+ans <- tapply(mtcars$hp, mtcars$cyl, mean)
+abs(ans['4'] - ans['8'])
